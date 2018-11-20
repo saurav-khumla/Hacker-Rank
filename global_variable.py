502273c7309331b845cb46d6651e4902801b9f95
saurav <30296973+saurav-khumla@users.noreply.github.com>
2018-11-20 22:53:53 +0530

    Delete global_variable.py

--- global_variable.py
+++
@@ -1,9 +0,0 @@
-f=101
-print(f)
-def fun1():
-    global f
-    print(f)    #global variable data work when function is called
-    f = 'string'
-
-fun1()   #it call the function
-print(f)   # it give updated string data.
