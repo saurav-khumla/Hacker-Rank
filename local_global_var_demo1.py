a644923f2a3ccc0253c80f19279c8dee72400cf7
saurav <30296973+saurav-khumla@users.noreply.github.com>
2018-11-20 22:54:15 +0530

    Delete local_global_var_demo1.py

--- local_global_var_demo1.py
+++
@@ -1,9 +0,0 @@
-f = 101 #global variable  and it will execute tfirst
-print(f)
-
-def fun1():
-    f='string'   #global f is now redeclare as local variable
-    print(f)     # run when fun1 will call
-
-fun1()    # calling function so it will run at 2nd
-print(f)    # run at 3rd
